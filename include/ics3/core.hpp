5dfe3fafffd0e66b071e36df2f240dbe64ebf5c9
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-09-30 06:55:02 +0900

    Add Core class

---
+++ include/ics3/core.hpp
@@ -0,0 +1,16 @@
+#ifndef LIBICS3_ICS3_CORE_H_
+#define LIBICS3_ICS3_CORE_H_
+
+#include <vector>
+
+namespace ics {
+  class Core {
+  public:
+    static const Core &getReference();
+    std::vector<unsigned char> &communicate(std::vector<unsigned char>);
+  private:
+    static const Core INSTANCE;
+  };
+}
+
+#endif // LIBICS3_ICS3_CORE_H_
