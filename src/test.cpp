e106de6436dbfcbaeb5e5bd2e98f814655b6031f
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-10-01 00:59:58 +0900

    Update test code

--- src/test.cpp
+++ src/test.cpp
@@ -14,9 +14,15 @@ int main(int argc, char **argv) {
     std::cout << degree.get() << ':' << degree.getRaw() << std::endl;
     degree.set(60);
     std::cout << degree.get() << ':' << degree.getRaw() << std::endl;
+    try {
+      degree.set(150);
+      std::cerr << "Never run this" << std::endl;
+    } catch (const std::invalid_argument &e) {
+      std::cout << e.what() << std::endl;
+    }
   }
   {
-    std::cout << "angle of radian test section" << std::endl;
+    std::cout << std::endl << "angle of radian test section" << std::endl;
     ics::Angle radian = ics::Angle::createRadian();
     radian.set(0);
     std::cout << radian.get() << ':' << radian.getRaw() << std::endl;
@@ -24,12 +30,24 @@ int main(int argc, char **argv) {
     std::cout << radian.get() << ':' << radian.getRaw() << std::endl;
     radian.set(M_PI / 3);
     std::cout << radian.get() << ':' << radian.getRaw() << std::endl;
+    try {
+      radian.set(M_PI);
+      std::cerr << "Never run this" << std::endl;
+    } catch (const std::invalid_argument &e) {
+      std::cout << e.what() << std::endl;
+    }
   }
   {
-    std::cout << "parameter test section" << std::endl;
+    std::cout << std::endl << "parameter test section" << std::endl;
     ics::Parameter current = ics::Parameter::current();
     current.set(30);
     std::cout << static_cast<int>(current.get()) << std::endl;
+    try {
+      current.set(70);
+      std::cerr << "Never run this" << std::endl;
+    } catch (const std::invalid_argument &e) {
+      std::cout << e.what() << std::endl;
+    }
   }
   return 0;
 }
