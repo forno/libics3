fe166dc1bfd9a434772292f05168be826068ecb3
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-09-30 23:52:45 +0900

    Add header file of Parameter class

---
+++ include/ics3/parameter.hpp
@@ -0,0 +1,25 @@
+#ifndef LIBICS3_ICS3_PARAMETER_H_
+#define LIBICS3_ICS3_PARAMETER_H_
+
+#include <stdexcept>
+
+namespace ics {
+  class Parameter {
+  public:
+    static Parameter stretch() noexcept;
+    static Parameter speed() noexcept;
+    static Parameter current() noexcept;
+    static Parameter temperature() noexcept;
+
+    unsigned char get() const noexcept;
+    void set(unsigned char) throw(std::invalid_argument);
+  private:
+    Parameter(unsigned char, unsigned char, unsigned char) noexcept;
+    const unsigned char sc;
+    const unsigned char min;
+    const unsigned char max;
+    unsigned char data;
+  };
+}
+
+#endif // LIBICS3_ICS3_PARAMETER_H_
