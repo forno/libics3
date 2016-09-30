8a64c62bf15f7f78cbff57b704902c4a5f427e33
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-09-30 15:29:25 +0900

    Update eeprom class for something

--- include/ics3/eeprom.hpp
+++ include/ics3/eeprom.hpp
@@ -1,69 +1,55 @@
 #ifndef LIBICS3_ICS3_EEPROM_H_
 #define LIBICS3_ICS3_EEPROM_H_
 
-#include <vector>
+#include <array>
 #include <stdexcept>
 
 namespace ics {
   class EepParam {
     public:
-      static EepParam strech();
-      static EepParam speed();
-      static EepParam punch();
-      static EepParam deadBand();
-      static EepParam dumping();
-      static EepParam selfTimer();
-      static EepParam pulseMaxLimit();
-      static EepParam pulseMinLimit();
-      static EepParam tmperatureLimit();
-      static EepParam currentLimit();
-      static EepParam response();
-      static EepParam userOffset();
-      static EepParam id();
-      static EepParam strech1();
-      static EepParam strech2();
-      static EepParam strech3();
+      static EepParam strech() noexcept;
+      static EepParam speed() noexcept;
+      static EepParam punch() noexcept;
+      static EepParam deadBand() noexcept;
+      static EepParam dumping() noexcept;
+      static EepParam selfTimer() noexcept;
+      static EepParam pulseMaxLimit() noexcept;
+      static EepParam pulseMinLimit() noexcept;
+      static EepParam tmperatureLimit() noexcept;
+      static EepParam currentLimit() noexcept;
+      static EepParam response() noexcept;
+      static EepParam userOffset() noexcept;
+      static EepParam id() noexcept;
+      static EepParam strech1() noexcept;
+      static EepParam strech2() noexcept;
+      static EepParam strech3() noexcept;
       void set(uint16_t) throw(std::range_error);
       uint16_t get() const noexcept;
-      void setRaw(std::vector<unsigned char> &) const noexcept;
+      void setRaw(std::array<unsigned char, 64> &) const noexcept;
     private:
-      EepParam(int, uint16_t, uint16_t, void *(std::vector<unsigned char> &), void *(uint16_t));
-      void setRaw2byte(std::vector<unsigned char> &) const noexcept;
-      void setRaw4byte(std::vector<unsigned char> &) const noexcept;
+      EepParam(int, uint16_t, uint16_t, void (*)(std::array<unsigned char, 64> &), void (*)(uint16_t)) noexcept;
+      void setRaw2byte(std::array<unsigned char, 64> &) const noexcept;
+      void setRaw4byte(std::array<unsigned char, 64> &) const noexcept;
       void setNormal(uint16_t) throw(std::range_error);
       void setEven(uint16_t) throw(std::range_error);
 
       const int offset;
       const uint16_t min;
       const uint16_t max;
-      void (*const rawFunc)(std::vector<unsigned char> &);
+      void (*const rawFunc)(std::array<unsigned char, 64> &);
       void (*const setFunc)(uint16_t);
       uint16_t data;
-
-      static const EepParam STRECH;
-      static const EepParam SPEED;
-      static const EepParam PUNCH;
-      static const EepParam DEAD_BAND;
-      static const EepParam DUMPING;
-      static const EepParam SELF_TIMER;
-      static const EepParam PULSE_MAX_LIMIT;
-      static const EepParam PULSE_MIN_LIMIT;
-      static const EepParam TMPERATURE_LIMIT;
-      static const EepParam CURRENT_LIMIT;
-      static const EepParam RESPONSE;
-      static const EepParam USER_OFFSET;
-      static const EepParam ID;
-      static const EepParam STRECH1;
-      static const EepParam STRECH2;
-      static const EepParam STRECH3;
   };
+
   class Eeprom {
   public:
     Eeprom();
     void set(EepParam);
     EepParam get(const EepParam &);
+    void copy(std::vector<unsigned char> &);
 
   private:
+    std::array<unsigned char, 64> data;
   };
 }
 
