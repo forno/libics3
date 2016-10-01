5add100b5eaa8e0ce24b1f038f78be2eceb43a7c
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-10-01 22:09:22 +0900

    Update testcode for use assert

--- src/test.cpp
+++ src/test.cpp
@@ -1,37 +1,31 @@
 #include <iostream>
 #define _USE_MATH_DEFINES
 #include <cmath>
+#include <cassert>
 
 #include "ics3/ics"
 
 int main(int argc, char **argv) {
-  std::cout << "angle of degree test section" << std::endl;
+  std::cout << "angle test section" << std::endl;
   {
     ics::Angle degree = ics::Angle::createDegree();
-    std::cout << degree.get() << ':' << degree.getRaw() << std::endl;
+    ics::Angle radian = ics::Angle::createRadian();
+    assert(degree.getRaw() == radian.getRaw());
     degree.set(0);
-    std::cout << degree.get() << ':' << degree.getRaw() << std::endl;
+    radian.set(0);
+    assert(degree.getRaw() == radian.getRaw());
     degree.set(90);
-    std::cout << degree.get() << ':' << degree.getRaw() << std::endl;
+    radian.set(M_PI / 2);
+    assert(degree.getRaw() == radian.getRaw());
     degree.set(60);
-    std::cout << degree.get() << ':' << degree.getRaw() << std::endl;
+    radian.set(M_PI / 3);
+    assert(degree.getRaw() == radian.getRaw());
     try {
       degree.set(150);
       std::cerr << "Never run this" << std::endl;
     } catch (const std::invalid_argument &e) {
       std::cout << e.what() << std::endl;
     }
-  }
-  {
-    std::cout << std::endl << "angle of radian test section" << std::endl;
-    ics::Angle radian = ics::Angle::createRadian();
-    std::cout << radian.get() << ':' << radian.getRaw() << std::endl;
-    radian.set(0);
-    std::cout << radian.get() << ':' << radian.getRaw() << std::endl;
-    radian.set(M_PI / 2);
-    std::cout << radian.get() << ':' << radian.getRaw() << std::endl;
-    radian.set(M_PI / 3);
-    std::cout << radian.get() << ':' << radian.getRaw() << std::endl;
     try {
       radian.set(M_PI);
       std::cerr << "Never run this" << std::endl;
@@ -42,9 +36,9 @@ int main(int argc, char **argv) {
   {
     std::cout << std::endl << "parameter test section" << std::endl;
     ics::Parameter current = ics::Parameter::current();
-    std::cout << static_cast<int>(current.get()) << std::endl;
+    assert(63 == current.get());
     current.set(30);
-    std::cout << static_cast<int>(current.get()) << std::endl;
+    assert(30 == current.get());
     try {
       current.set(70);
       std::cerr << "Never run this" << std::endl;
@@ -55,9 +49,9 @@ int main(int argc, char **argv) {
   {
     std::cout << std::endl << "EepParam test section" << std::endl;
     ics::EepParam speed = ics::EepParam::speed();
-    std::cout << speed.get() << std::endl;
+    assert(127 == speed.get());
     speed.set(100);
-    std::cout << speed.get() << std::endl;
+    assert(100 == speed.get());
     try {
       speed.set(200);
       std::cerr << "Never run this" << std::endl;
