8ca90c8e56b02b77af894e3883dfdcf55423b82b
YamaCIR-KIT <httperror@404-notfound.jp>
2016-10-06 18:58:07 +0900

    バグ取りしてCMakeLists.txt追加

--- include/ics3/ics3.hpp
+++ include/ics3/ics3.hpp
@@ -15,20 +15,20 @@ namespace ics {
 
   class ICS3 {
   public:
-    enum Baudrate : speed_t {
+    enum ICSBaudrate : speed_t {
       RATE115200 = B115200,
       //RATE625000 = B625000,
       //RATE1250000 = B1250000
     };
 
-    ICS3(const char *, Baudrate) throw(std::invalid_argument, std::runtime_error);
-    Angle move(const ID &, Angle) const throw(std::runtime_error);
-    Parameter get(const ID &, Parameter) const throw(std::runtime_error);
-    void set(const ID &, const Parameter &) const throw(std::runtime_error);
-    Eeprom getRom(const ID &) const throw(std::runtime_error);
-    void setRom(const ID &, const Eeprom &) const throw(std::runtime_error);
+    ICS3(const char*, ICSBaudrate) throw(std::invalid_argument, std::runtime_error);
+    Angle move(const ID&, Angle) const throw(std::runtime_error);
+    Parameter get(const ID&, Parameter) const throw(std::runtime_error);
+    void set(const ID&, const Parameter&) const throw(std::runtime_error);
+    Eeprom getRom(const ID&) const throw(std::runtime_error);
+    void setRom(const ID&, const Eeprom&) const throw(std::runtime_error);
   private:
-    const Core &core;
+    const Core& core;
   };
 }
 
