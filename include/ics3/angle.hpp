cb4ab7f62c3ae46ab6f01d27d07a84a4a451eba2
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-10-15 17:17:54 +0900

    Update base system of angle class

--- include/ics3/angle.hpp
+++ include/ics3/angle.hpp
@@ -8,18 +8,18 @@ namespace ics {
   public:
     static Angle newDegree() noexcept;
     static Angle newRadian() noexcept;
+    static const uint16_t MIN;
+    static const uint16_t MAX;
 
     double get() const noexcept;
     void set(double);
     uint16_t getRaw() const noexcept;
     void setRaw(uint16_t);
   private:
-    Angle(double, double, double) noexcept;
+    Angle(double) noexcept;
 
-    const double min;
-    const double max;
     const double rawCalibration;
-    double data;
+    uint16_t rawData;
   };
 }
 
--- src/angle.cpp
+++ src/angle.cpp
@@ -4,42 +4,39 @@
 #include <cmath>
 
 ics::Angle ics::Angle::newDegree() noexcept {
-  constexpr double limit = 130;
-  static const Angle DEGREE(-limit, limit, 800.0 / 27.0);
-  return DEGREE;
+  return Angle(800.0 / 27.0);
 }
 
 ics::Angle ics::Angle::newRadian() noexcept {
-  constexpr double limit = 130 * M_PI / 180;
-  static const Angle RADIAN(-limit, limit, 16000.0 / 3.0 / M_PI);
-  return RADIAN;
+  return Angle(16000.0 / 3.0 / M_PI);
 }
 
+const uint16_t ics::Angle::MIN = 3500;
+const uint16_t ics::Angle::MAX = 11500;
+
 double ics::Angle::get() const noexcept {
-  return data;
+  return (rawData - 7500) / rawCalibration;
 }
 
 void ics::Angle::set(double angle) {
-  if (angle < min) throw std::invalid_argument("Too small angle");
-  if (max < angle) throw std::invalid_argument("Too big angle");
-  data = angle;
-}
-
-uint16_t ics::Angle::getRaw() const noexcept {
-  return static_cast<uint16_t>(data * rawCalibration + 7500);
-}
-
-void ics::Angle::setRaw(uint16_t raw) {
   try {
-    set((raw - 7500) / rawCalibration);
-  } catch (std::invalid_argument& e) {
+    setRaw(static_cast<uint16_t>(angle * rawCalibration + 7500));
+  } catch (...) {
     throw;
   }
 }
 
-ics::Angle::Angle(double min, double max, double calibration) noexcept
-: min(min),
-  max(max),
-  rawCalibration(calibration),
-  data(0)
+uint16_t ics::Angle::getRaw() const noexcept {
+  return rawData;
+}
+
+void ics::Angle::setRaw(uint16_t raw) {
+  if (raw < MIN) throw std::invalid_argument("Too min angle");
+  if (MAX < raw) throw std::invalid_argument("Too big angle");
+  rawData = raw;
+}
+
+ics::Angle::Angle(double calibration) noexcept
+: rawCalibration(calibration),
+  rawData(7500)
 {}
