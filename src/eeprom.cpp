0a60702e1a2688b4f7143a1fe61e27cad88bfb4a
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-10-17 01:05:52 +0900

    Remove check invalid self; it only created ICS3

--- src/eeprom.cpp
+++ src/eeprom.cpp
@@ -1,16 +1,11 @@
 #include "ics3/eeprom.hpp"
 
 #include "ics3/eepparam.hpp"
-#include "ics3/ics3.hpp"
 
 #include <stdexcept>
 
 ics::EepParam ics::Eeprom::get(EepParam param) const {
-  try {
-    param.read(data);
-  } catch (...) {
-    throw std::runtime_error {"Fail data: non initialize EEPROM by ICS"};
-  }
+  param.read(data);
   return param;
 }
 
