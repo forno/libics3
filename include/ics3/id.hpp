60bc1b7afddfd18fbe0a948dd1e0c18c1449d1fc
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-10-17 03:04:53 +0900

    Add cast method of unsigned char on ID

--- include/ics3/id.hpp
+++ include/ics3/id.hpp
@@ -7,6 +7,7 @@ namespace ics {
   class ID {
   public:
     constexpr ID(unsigned char);
+    constexpr operator unsigned char() const noexcept;
     constexpr unsigned char get() const noexcept;
   private:
     const unsigned char data;
@@ -15,6 +16,10 @@ namespace ics {
   constexpr ID::ID(unsigned char id)
   : data {id < 32 ? id : throw std::invalid_argument {"Too big arguments"}}
   {}
+
+  constexpr ID::operator unsigned char() const noexcept {
+    return data;
+  }
 
   constexpr unsigned char ID::get() const noexcept {
     return data;
