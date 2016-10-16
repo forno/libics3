b5810135a68fb836d359b3a69af8898aa34675fb
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-10-17 04:35:16 +0900

    Refactor get and unsigned char() method on ID

--- include/ics3/id.hpp
+++ include/ics3/id.hpp
@@ -7,8 +7,8 @@ namespace ics {
   class ID {
   public:
     constexpr ID(unsigned char);
-    constexpr operator unsigned char() const noexcept;
     constexpr unsigned char get() const noexcept;
+    constexpr operator unsigned char() const noexcept;
   private:
     const unsigned char data;
   };
@@ -17,12 +17,12 @@ namespace ics {
   : data {id < 32 ? id : throw std::invalid_argument {"Too big arguments"}}
   {}
 
-  constexpr ID::operator unsigned char() const noexcept {
+  constexpr unsigned char ID::get() const noexcept {
     return data;
   }
 
-  constexpr unsigned char ID::get() const noexcept {
-    return data;
+  constexpr ID::operator unsigned char() const noexcept {
+    return get();
   }
 }
 
