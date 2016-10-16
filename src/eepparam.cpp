de1523ea38a677b45db493701b593aa1c6a24bb8
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-10-16 22:14:15 +0900

    Update init variable on eepparam

--- src/eepparam.cpp
+++ src/eepparam.cpp
@@ -1,93 +1,75 @@
 #include "ics3/eepparam.hpp"
 
 ics::EepParam ics::EepParam::strech() noexcept {
-  static const EepParam STRECH(2, 2, 2, 254, &ics::EepParam::setEven, 60);
-  return STRECH;
+  return EepParam {2, 2, 2, 254, &ics::EepParam::setEven, 60};
 }
 
 ics::EepParam ics::EepParam::speed() noexcept {
-  static const EepParam SPEED(4, 2, 1, 127, &ics::EepParam::setNormal, 127);
-  return SPEED;
+  return EepParam {4, 2, 1, 127, &ics::EepParam::setNormal, 127};
 }
 
 ics::EepParam ics::EepParam::punch() noexcept {
-  static const EepParam PUNCH(6, 2, 0, 10, &ics::EepParam::setNormal, 1);
-  return PUNCH;
+  return EepParam {6, 2, 0, 10, &ics::EepParam::setNormal, 1};
 }
 
 ics::EepParam ics::EepParam::deadBand() noexcept {
-  static const EepParam DEAD_BAND(8, 2, 0, 5, &ics::EepParam::setNormal, 2);
-  return DEAD_BAND;
+  return EepParam {8, 2, 0, 5, &ics::EepParam::setNormal, 2};
 }
 
 ics::EepParam ics::EepParam::dumping() noexcept {
-  static const EepParam DUMPING(10, 2, 1, 255, &ics::EepParam::setNormal, 40);
-  return DUMPING;
+  return EepParam {10, 2, 1, 255, &ics::EepParam::setNormal, 40};
 }
 
 ics::EepParam ics::EepParam::selfTimer() noexcept {
-  static const EepParam SELF_TIMER(12, 2, 10, 255, &ics::EepParam::setNormal, 250);
-  return SELF_TIMER;
+  return EepParam {12, 2, 10, 255, &ics::EepParam::setNormal, 250};
 }
 
 ics::EepParam ics::EepParam::flag() noexcept {
-  static const EepParam FLAG(14, 2, 0, 255, &ics::EepParam::setFlag, 0x8C);
-  return FLAG;
+  return EepParam {14, 2, 0, 255, &ics::EepParam::setFlag, 0x8C};
 }
 
 ics::EepParam ics::EepParam::pulseMax() noexcept {
-  static const EepParam PULSE_MAX(16, 4, 3500, 11500, &ics::EepParam::setNormal, 11500);
-  return PULSE_MAX;
+  return EepParam {16, 4, 3500, 11500, &ics::EepParam::setNormal, 11500};
 }
 
 ics::EepParam ics::EepParam::pulseMin() noexcept {
-  static const EepParam PULSE_MIN(20, 4, 3500, 11500, &ics::EepParam::setNormal, 3500);
-  return PULSE_MIN;
+  return EepParam {20, 4, 3500, 11500, &ics::EepParam::setNormal, 3500};
 }
 
 ics::EepParam ics::EepParam::baudrate() noexcept {
-  static const EepParam BAUDRATE(26, 2, 0, 10, &ics::EepParam::setBaudrate, 10);
-  return BAUDRATE;
+  return EepParam {26, 2, 0, 10, &ics::EepParam::setBaudrate, 10};
 }
 
 ics::EepParam ics::EepParam::temperature() noexcept {
-  static const EepParam TEMPERATURE(28, 2, 1, 127, &ics::EepParam::setNormal, 80);
-  return TEMPERATURE;
+  return EepParam {28, 2, 1, 127, &ics::EepParam::setNormal, 80};
 }
 
 ics::EepParam ics::EepParam::current() noexcept {
-  static const EepParam CURRENT(30, 2, 1, 63, &ics::EepParam::setNormal, 63);
-  return CURRENT;
+  return EepParam {30, 2, 1, 63, &ics::EepParam::setNormal, 63};
 }
 
 ics::EepParam ics::EepParam::response() noexcept {
-  static const EepParam RESPONSE(50, 2, 1, 5, &ics::EepParam::setNormal, 3);
-  return RESPONSE;
+  return EepParam {50, 2, 1, 5, &ics::EepParam::setNormal, 3};
 }
 
 ics::EepParam ics::EepParam::userOffset() noexcept {
-  static const EepParam USER_OFFSET(52, 2, -127, 127, &ics::EepParam::setOffset, 0);
-  return USER_OFFSET;
+  return EepParam {52, 2, static_cast<uint16_t>(-127), 127, &ics::EepParam::setOffset, 0};
 }
 
 ics::EepParam ics::EepParam::id() noexcept {
-  static const EepParam ID(56, 2, 0, 31, &ics::EepParam::setNormal, 0);
-  return ID;
+  return EepParam {56, 2, 0, 31, &ics::EepParam::setNormal, 0};
 }
 
 ics::EepParam ics::EepParam::strech1() noexcept {
-  static const EepParam STRECH1(58, 2, 2, 254, &ics::EepParam::setEven, 60);
-  return STRECH1;
+  return EepParam {58, 2, 2, 254, &ics::EepParam::setEven, 60};
 }
 
 ics::EepParam ics::EepParam::strech2() noexcept {
-  static const EepParam STRECH2(60, 2, 2, 254, &ics::EepParam::setEven, 60);
-  return STRECH2;
+  return EepParam {60, 2, 2, 254, &ics::EepParam::setEven, 60};
 }
 
 ics::EepParam ics::EepParam::strech3() noexcept {
-  static const EepParam STRECH3(62, 2, 2, 254, &ics::EepParam::setEven, 60);
-  return STRECH3;
+  return EepParam {62, 2, 2, 254, &ics::EepParam::setEven, 60};
 }
 
 uint16_t ics::EepParam::get() const noexcept {
@@ -99,17 +81,17 @@ void ics::EepParam::set(uint16_t input) {
 }
 
 void ics::EepParam::write(std::array<unsigned char, 64>& dest) const noexcept {
-  uint16_t mask = 0xF;
-  for (size_t i = offset + length - 1; i >= offset; i--) {
+  uint16_t mask {0xF};
+  for (size_t i {offset + length - 1}; i >= offset; i--) {
     dest[i] = data & mask;
     mask <<= 4;
   }
 }
 
 void ics::EepParam::read(const std::array<unsigned char, 64>& src) {
-  uint16_t result = 0;
-  uint16_t mask = 0xF;
-  for (size_t i = offset + length - 1; i >= offset; i--) {
+  uint16_t result {0};
+  uint16_t mask {0xF};
+  for (size_t i {offset + length - 1}; i >= offset; i--) {
     result |= src[i] & mask;
     mask <<= 4;
   }
@@ -160,8 +142,8 @@ void ics::EepParam::setBaudrate(uint16_t input) {
 }
 
 void ics::EepParam::setOffset(uint16_t input) {
-  static const int8_t minBuf = min; // I expect stand 0x8000 input cast to minus value
-  int8_t inputBuf = input; // I expect stand 0x8000 input cast to minus value
+  static const int8_t minBuf {static_cast<int8_t>(min)}; // I expect stand 0x8000 input cast to minus value
+  int8_t inputBuf {static_cast<int8_t>(input)}; // I expect stand 0x8000 input cast to minus value
   if (inputBuf < minBuf) throw std::invalid_argument("Too small value");
   if (max < inputBuf) throw std::invalid_argument("Too big value");
   data = static_cast<uint8_t>(inputBuf);
