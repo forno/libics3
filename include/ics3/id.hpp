7454567b33e28b6f8414ef83270414de99a1ccb2
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-10-17 02:48:20 +0900

    Refactor ID class convert to constexpr

--- include/ics3/id.hpp
+++ include/ics3/id.hpp
@@ -1,14 +1,24 @@
 #ifndef LIBICS3_ICS3_ID_H_
 #define LIBICS3_ICS3_ID_H_
 
+#include<stdexcept>
+
 namespace ics {
   class ID {
   public:
-    ID(unsigned char);
-    unsigned char get() const noexcept;
+    constexpr ID(unsigned char);
+    constexpr unsigned char get() const noexcept;
   private:
     const unsigned char data;
   };
+
+  constexpr ID::ID(unsigned char id)
+  : data {id < 32 ? id : throw std::invalid_argument {"Too big arguments"}}
+  {}
+
+  constexpr unsigned char ID::get() const noexcept {
+    return data;
+  }
 }
 
 #endif // LIBICS3_ICS3_ID_H_
