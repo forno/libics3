de00001f7fc390b105fc9a146c49c2520d7d00da
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-10-17 18:28:55 +0900

    Fix tirmios init

--- src/core.cpp
+++ src/core.cpp
@@ -38,7 +38,7 @@ ics::Core::Core(const char* path, speed_t baudrate)
       throw std::invalid_argument {"Not tty device"};
     if (tcgetattr(fd, &oldTio) < 0)
       throw std::runtime_error {"Cannot setup tty"};
-    termios newTio {getTermios()};
+    auto newTio = getTermios();
     if (cfsetispeed(&newTio, baudrate) < 0)
       throw std::runtime_error {"Cannot set baudrate"};
     if (cfsetospeed(&newTio, baudrate) < 0)
