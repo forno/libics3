88c92fae471debaa7feec521eb79b9bd63242d79
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-10-17 16:12:50 +0900

    Update Parameter class apply constexpr

--- include/ics3/parameter.hpp
+++ include/ics3/parameter.hpp
@@ -4,21 +4,45 @@
 namespace ics {
   class Parameter {
   public:
-    static Parameter stretch() noexcept;
-    static Parameter speed() noexcept;
-    static Parameter current() noexcept;
-    static Parameter temperature() noexcept;
+    static constexpr Parameter stretch() noexcept;
+    static constexpr Parameter speed() noexcept;
+    static constexpr Parameter current() noexcept;
+    static constexpr Parameter temperature() noexcept;
 
     unsigned char get() const noexcept;
     void set(unsigned char);
     unsigned char getSc() const noexcept;
   private:
-    explicit Parameter(unsigned char, unsigned char, unsigned char, unsigned char) noexcept;
+    constexpr explicit Parameter(unsigned char, unsigned char, unsigned char, unsigned char) noexcept;
+
     const unsigned char sc;
     const unsigned char min;
     const unsigned char max;
     unsigned char data;
   };
+
+  constexpr Parameter Parameter::stretch() noexcept {
+    return Parameter {0x01, 1, 127, 30};
+  }
+
+  constexpr Parameter Parameter::speed() noexcept {
+    return Parameter {0x02, 1, 127, 127};
+  }
+
+  constexpr Parameter Parameter::current() noexcept {
+    return Parameter {0x03, 0, 63, 63};
+  }
+
+  constexpr Parameter Parameter::temperature() noexcept {
+    return Parameter {0x04, 1, 127, 80};
+  }
+
+  constexpr Parameter::Parameter(unsigned char sc, unsigned char min, unsigned char max, unsigned char default_data) noexcept
+  : sc {sc},
+    min {min},
+    max {max},
+    data {default_data}
+  {}
 }
 
 #endif // LIBICS3_ICS3_PARAMETER_H_
--- src/parameter.cpp
+++ src/parameter.cpp
@@ -2,22 +2,6 @@
 
 #include<stdexcept>
 
-ics::Parameter ics::Parameter::stretch() noexcept {
-  return Parameter {0x01, 1, 127, 30};
-}
-
-ics::Parameter ics::Parameter::speed() noexcept {
-  return Parameter {0x02, 1, 127, 127};
-}
-
-ics::Parameter ics::Parameter::current() noexcept {
-  return Parameter {0x03, 0, 63, 63};
-}
-
-ics::Parameter ics::Parameter::temperature() noexcept {
-  return Parameter {0x04, 1, 127, 80};
-}
-
 unsigned char ics::Parameter::get() const noexcept {
   return data;
 }
@@ -31,10 +15,3 @@ void ics::Parameter::set(unsigned char input) {
 unsigned char ics::Parameter::getSc() const noexcept {
   return sc;
 }
-
-ics::Parameter::Parameter(unsigned char sc, unsigned char min, unsigned char max, unsigned char default_data) noexcept
-: sc {sc},
-  min {min},
-  max {max},
-  data {default_data}
-{}
