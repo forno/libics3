f059523b73c0fbf33e7a688cef7ce8a477e36123
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-10-17 23:38:41 +0900

    Update setup.sh

--- script/setup.sh
+++ script/setup.sh
@@ -1,3 +1,4 @@
 sudo modprobe ftdi-sio
 sudo chmod 666 /sys/bus/usb-serial/drivers/ftdi_sio/new_id
 echo 165c 0008 > /sys/bus/usb-serial/drivers/ftdi_sio/new_id
+echo "ttyUSB{x} created : {x} is number."
