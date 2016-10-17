2d349320c921f0d042c12b52c0d106609f828f6c
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-10-17 21:39:37 +0900

    Update static factory of eepparam with default arg

--- include/ics3/eepparam.hpp
+++ include/ics3/eepparam.hpp
@@ -19,24 +19,24 @@ namespace ics {
       RATE1250000 = 0
     };
 
-    static constexpr EepParam strech() noexcept;
-    static constexpr EepParam speed() noexcept;
-    static constexpr EepParam punch() noexcept;
-    static constexpr EepParam deadBand() noexcept;
-    static constexpr EepParam dumping() noexcept;
-    static constexpr EepParam selfTimer() noexcept;
-    static constexpr EepParam flag() noexcept;
-    static constexpr EepParam pulseMax() noexcept;
-    static constexpr EepParam pulseMin() noexcept;
-    static constexpr EepParam baudrate() noexcept;
-    static constexpr EepParam temperature() noexcept;
-    static constexpr EepParam current() noexcept;
-    static constexpr EepParam response() noexcept;
-    static constexpr EepParam userOffset() noexcept;
-    static constexpr EepParam id() noexcept;
-    static constexpr EepParam strech1() noexcept;
-    static constexpr EepParam strech2() noexcept;
-    static constexpr EepParam strech3() noexcept;
+    static constexpr EepParam strech(uint16_t = 60) noexcept;
+    static constexpr EepParam speed(uint16_t = 127) noexcept;
+    static constexpr EepParam punch(uint16_t = 1) noexcept;
+    static constexpr EepParam deadBand(uint16_t = 2) noexcept;
+    static constexpr EepParam dumping(uint16_t = 40) noexcept;
+    static constexpr EepParam selfTimer(uint16_t = 250) noexcept;
+    static constexpr EepParam flag(uint16_t = 0x8C) noexcept;
+    static constexpr EepParam pulseMax(uint16_t = 11500) noexcept;
+    static constexpr EepParam pulseMin(uint16_t = 3500) noexcept;
+    static constexpr EepParam baudrate(uint16_t = 10) noexcept;
+    static constexpr EepParam temperature(uint16_t = 80) noexcept;
+    static constexpr EepParam current(uint16_t = 63) noexcept;
+    static constexpr EepParam response(uint16_t = 0) noexcept;
+    static constexpr EepParam userOffset(uint16_t = 0) noexcept;
+    static constexpr EepParam id(uint16_t = 0) noexcept;
+    static constexpr EepParam strech1(uint16_t = 60) noexcept;
+    static constexpr EepParam strech2(uint16_t = 60) noexcept;
+    static constexpr EepParam strech3(uint16_t = 60) noexcept;
 
     constexpr uint16_t get() const noexcept;
     void set(uint16_t);
@@ -65,76 +65,76 @@ namespace ics {
     uint16_t data;
   };
 
-  constexpr EepParam EepParam::strech() noexcept {
-    return EepParam {2, 2, 2, 254, &EepParam::setEven, 60};
+  constexpr EepParam EepParam::strech(uint16_t data) noexcept {
+    return EepParam {2, 2, 2, 254, &EepParam::setEven, data};
   }
 
-  constexpr EepParam EepParam::speed() noexcept {
-    return EepParam {4, 2, 1, 127, &EepParam::setNormal, 127};
+  constexpr EepParam EepParam::speed(uint16_t data) noexcept {
+    return EepParam {4, 2, 1, 127, &EepParam::setNormal, data};
   }
 
-  constexpr EepParam EepParam::punch() noexcept {
-    return EepParam {6, 2, 0, 10, &EepParam::setNormal, 1};
+  constexpr EepParam EepParam::punch(uint16_t data) noexcept {
+    return EepParam {6, 2, 0, 10, &EepParam::setNormal, data};
   }
 
-  constexpr EepParam EepParam::deadBand() noexcept {
-    return EepParam {8, 2, 0, 5, &EepParam::setNormal, 2};
+  constexpr EepParam EepParam::deadBand(uint16_t data) noexcept {
+    return EepParam {8, 2, 0, 5, &EepParam::setNormal, data};
   }
 
-  constexpr EepParam EepParam::dumping() noexcept {
-    return EepParam {10, 2, 1, 255, &EepParam::setNormal, 40};
+  constexpr EepParam EepParam::dumping(uint16_t data) noexcept {
+    return EepParam {10, 2, 1, 255, &EepParam::setNormal, data};
   }
 
-  constexpr EepParam EepParam::selfTimer() noexcept {
-    return EepParam {12, 2, 10, 255, &EepParam::setNormal, 250};
+  constexpr EepParam EepParam::selfTimer(uint16_t data) noexcept {
+    return EepParam {12, 2, 10, 255, &EepParam::setNormal, data};
   }
 
-  constexpr EepParam EepParam::flag() noexcept {
-    return EepParam {14, 2, 0, 255, &EepParam::setFlag, 0x8C};
+  constexpr EepParam EepParam::flag(uint16_t data) noexcept {
+    return EepParam {14, 2, 0, 255, &EepParam::setFlag, data};
   }
 
-  constexpr EepParam EepParam::pulseMax() noexcept {
-    return EepParam {16, 4, 3500, 11500, &EepParam::setNormal, 11500};
+  constexpr EepParam EepParam::pulseMax(uint16_t data) noexcept {
+    return EepParam {16, 4, 3500, 11500, &EepParam::setNormal, data};
   }
 
-  constexpr EepParam EepParam::pulseMin() noexcept {
-    return EepParam {20, 4, 3500, 11500, &EepParam::setNormal, 3500};
+  constexpr EepParam EepParam::pulseMin(uint16_t data) noexcept {
+    return EepParam {20, 4, 3500, 11500, &EepParam::setNormal, data};
   }
 
-  constexpr EepParam EepParam::baudrate() noexcept {
-    return EepParam {26, 2, 0, 10, &EepParam::setBaudrate, 10};
+  constexpr EepParam EepParam::baudrate(uint16_t data) noexcept {
+    return EepParam {26, 2, 0, 10, &EepParam::setBaudrate, data};
   }
 
-  constexpr EepParam EepParam::temperature() noexcept {
-    return EepParam {28, 2, 1, 127, &EepParam::setNormal, 80};
+  constexpr EepParam EepParam::temperature(uint16_t data) noexcept {
+    return EepParam {28, 2, 1, 127, &EepParam::setNormal, data};
   }
 
-  constexpr EepParam EepParam::current() noexcept {
-    return EepParam {30, 2, 1, 63, &EepParam::setNormal, 63};
+  constexpr EepParam EepParam::current(uint16_t data) noexcept {
+    return EepParam {30, 2, 1, 63, &EepParam::setNormal, data};
   }
 
-  constexpr EepParam EepParam::response() noexcept {
-    return EepParam {50, 2, 1, 5, &EepParam::setNormal, 3};
+  constexpr EepParam EepParam::response(uint16_t data) noexcept {
+    return EepParam {50, 2, 1, 5, &EepParam::setNormal, data};
   }
 
-  constexpr EepParam EepParam::userOffset() noexcept {
-    return EepParam {52, 2, static_cast<uint16_t>(-127), 127, &EepParam::setOffset, 0};
+  constexpr EepParam EepParam::userOffset(uint16_t data) noexcept {
+    return EepParam {52, 2, static_cast<uint16_t>(-127), 127, &EepParam::setOffset, data};
   }
 
-  constexpr EepParam EepParam::id() noexcept {
-    return EepParam {56, 2, 0, 31, &EepParam::setNormal, 0};
+  constexpr EepParam EepParam::id(uint16_t data) noexcept {
+    return EepParam {56, 2, 0, 31, &EepParam::setNormal, data};
   }
 
-  constexpr EepParam EepParam::strech1() noexcept {
-    return EepParam {58, 2, 2, 254, &EepParam::setEven, 60};
+  constexpr EepParam EepParam::strech1(uint16_t data) noexcept {
+    return EepParam {58, 2, 2, 254, &EepParam::setEven, data};
   }
 
-  constexpr EepParam EepParam::strech2() noexcept {
-    return EepParam {60, 2, 2, 254, &EepParam::setEven, 60};
+  constexpr EepParam EepParam::strech2(uint16_t data) noexcept {
+    return EepParam {60, 2, 2, 254, &EepParam::setEven, data};
   }
 
-  constexpr EepParam EepParam::strech3() noexcept {
-    return EepParam {62, 2, 2, 254, &EepParam::setEven, 60};
+  constexpr EepParam EepParam::strech3(uint16_t data) noexcept {
+    return EepParam {62, 2, 2, 254, &EepParam::setEven, data};
   }
 
   constexpr uint16_t EepParam::get() const noexcept {
