4e58c5cf95f5463dec6791bc9ff3df6f62fd5c9d
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-10-20 18:09:18 +0900

    Reformat indent of checkInvalidRange

--- include/ics3/check_invalid.hpp
+++ include/ics3/check_invalid.hpp
@@ -6,8 +6,8 @@
 namespace ics {
   template<typename T> constexpr T checkInvalidRange(T input, T min, T max) {
     return input < min ? throw std::invalid_argument {"Too small argument"} :
-      max < input ? throw std::invalid_argument {"Too big argument"} :
-      input;
+           max < input ? throw std::invalid_argument {"Too big argument"} :
+           input;
   }
 }
 
