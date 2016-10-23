8f1cb4027242c428efed5db8cd83ed36f6e11488
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-10-23 19:51:51 +0900

    Fix read eeprom error

--- src/eepparam.cpp
+++ src/eepparam.cpp
@@ -2,20 +2,20 @@
 
 #include<stdexcept>
 
+constexpr int byteSize {4};
+
 void ics::EepParam::write(std::array<unsigned char, 64>& dest) const noexcept {
   uint16_t mask {0xF};
-  for (size_t i {offset + length - 1}; i >= offset; i--) {
+  for (size_t i {offset + length - 1}; i >= offset; --i) {
     dest[i] = data & mask;
-    mask <<= 4;
+    mask <<= byteSize;
   }
 }
 
 void ics::EepParam::read(const std::array<unsigned char, 64>& src) {
+  constexpr uint16_t mask {0xF};
   uint16_t result {0};
-  uint16_t mask {0xF};
-  for (size_t i {offset + length - 1}; i >= offset; i--) {
-    result |= src[i] & mask;
-    mask <<= 4;
-  }
+  for (size_t i {0}; i < length; ++i)
+    result |= (src[offset + length - 1 - i] & mask) << (i * byteSize);
   set(result); // throw std::invalid_argument
 }
