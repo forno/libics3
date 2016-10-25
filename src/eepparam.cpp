5d9d82e8dad36510f566e9dff2f62c14ebb4804b
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-10-25 19:16:57 +0900

    Update read method for readability

--- src/eepparam.cpp
+++ src/eepparam.cpp
@@ -15,7 +15,10 @@ void ics::EepParam::write(std::array<uint8_t, 64>& dest) const noexcept {
 
 void ics::EepParam::read(const std::array<uint8_t, 64>& src) {
   uint16_t result {0};
-  for (size_t i {0}; i < length; ++i)
-    result |= (src[offset + length - 1 - i] & mask) << (i * byteSize);
+  const size_t loopend = offset + length;
+  for (size_t i {offset}; i < loopend; ++i) {
+    result <<= byteSize;
+    result |= src[i] & mask;
+  }
   set(result); // throw std::invalid_argument
 }
