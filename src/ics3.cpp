afc07a7207fa355a102e32f105a1ec873584c180
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-10-25 19:01:21 +0900

    Remove assign 0 to std::vector

--- src/ics3.cpp
+++ src/ics3.cpp
@@ -30,9 +30,7 @@ ics::Angle ics::ICS3::move(const ID& id, Angle angle) const {
 
 ics::Angle ics::ICS3::free(const ID& id, Angle unit) const {
   static std::vector<uint8_t> tx(3), rx(6);
-  tx[0] = 0x80 | id.get();
-  tx[1] = 0;
-  tx[2] = 0;
+  tx[0] = 0x80 | id.get(); // tx[1] == tx[2] == 0
   core.communicate(tx, rx); // throw std::runtime_error
   unit.rawData = getReceiveAngle(rx);
   return unit;
@@ -56,8 +54,7 @@ void ics::ICS3::set(const ID& id, const Parameter& param) const {
 
 ics::EepRom ics::ICS3::getRom(const ID& id) const {
   static std::vector<uint8_t> tx(2), rx(68);
-  tx[0] = 0xA0 | id.get();
-  tx[1] = 0;
+  tx[0] = 0xA0 | id.get(); // tx[1] == 0
   core.communicate(tx, rx); // throw std::runtime_error
   std::array<uint8_t, 64> romData;
   std::copy(rx.begin() + 4, rx.end(), romData.begin());
@@ -66,8 +63,7 @@ ics::EepRom ics::ICS3::getRom(const ID& id) const {
 
 void ics::ICS3::setRom(const ID& id, const EepRom& rom) const {
   static std::vector<uint8_t> tx(66), rx(68);
-  tx[0] = 0xC0 | id.get();
-  tx[1] = 0;
+  tx[0] = 0xC0 | id.get(); // tx[1] == 0
   rom.write(tx.begin() + 2);
   core.communicate(tx, rx); // throw std::runtime_error
 }
