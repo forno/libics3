05c16dfc648e20cc4e3e0feb5a7e14816f28ce82
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-11-03 15:22:20 +0900

    Add alias of interface type

--- include/ics3/angle.hpp
+++ include/ics3/angle.hpp
@@ -9,45 +9,46 @@ namespace ics {
     friend ICS3; // for touch setRaw
   public:
     using rawType = uint16_t;
-    static constexpr Angle newDegree(double = 0.0);
-    static constexpr Angle newRadian(double = 0.0);
-    static constexpr Angle newSameUnit(const Angle&, double = 0.0);
-    static constexpr Angle newCalibration(double, double = 0.0);
+    using type = double;
+    static constexpr Angle newDegree(type = 0.0);
+    static constexpr Angle newRadian(type = 0.0);
+    static constexpr Angle newSameUnit(const Angle&, type = 0.0);
+    static constexpr Angle newCalibration(type, type = 0.0);
     static constexpr rawType MIN {3500};
     static constexpr rawType MID {7500};
     static constexpr rawType MAX {11500};
-    static constexpr double PI {3.141592653589793};
+    static constexpr type PI {3.141592653589793};
 
     Angle(const Angle&) noexcept = default;
     Angle& operator=(const Angle&) noexcept;
-    constexpr double get() const noexcept;
-    constexpr operator double() const noexcept;
-    void set(double);
-    Angle& operator=(double);
+    constexpr type get() const noexcept;
+    constexpr operator type() const noexcept;
+    void set(type);
+    Angle& operator=(type);
     constexpr rawType getRaw() const noexcept;
     void setRaw(rawType);
   private:
-    constexpr explicit Angle(double, double);
-    static constexpr rawType castToRaw(double, double) noexcept;
+    constexpr explicit Angle(type, type);
+    static constexpr rawType castToRaw(type, type) noexcept;
     static constexpr rawType checkInvalidAngle(rawType);
 
     rawType rawData;
-    const double rawCalibration;
+    const type rawCalibration;
   };
 
-  constexpr Angle Angle::newDegree(double angle) {
+  constexpr Angle Angle::newDegree(type angle) {
     return Angle {800.0 / 27.0, angle};
   }
 
-  constexpr Angle Angle::newRadian(double angle) {
+  constexpr Angle Angle::newRadian(type angle) {
     return Angle {16000.0 / 3.0 / PI, angle};
   }
 
-  constexpr Angle Angle::newSameUnit(const Angle& unit, double angle) {
+  constexpr Angle Angle::newSameUnit(const Angle& unit, type angle) {
     return Angle {unit.rawCalibration, angle};
   }
 
-  constexpr Angle Angle::newCalibration(double calibration, double angle) {
+  constexpr Angle Angle::newCalibration(type calibration, type angle) {
     return Angle {calibration, angle};
   }
 
@@ -56,19 +57,19 @@ namespace ics {
     return *this;
   }
 
-  constexpr double Angle::get() const noexcept {
+  constexpr Angle::type Angle::get() const noexcept {
     return (rawData - MID) / rawCalibration;
   }
 
-  constexpr Angle::operator double() const noexcept {
+  constexpr Angle::operator type() const noexcept {
     return get();
   }
 
-  inline void Angle::set(double angle) {
+  inline void Angle::set(type angle) {
     setRaw(castToRaw(rawCalibration, angle)); // throw std::out_of_range
   }
 
-  inline Angle& Angle::operator=(double angle) {
+  inline Angle& Angle::operator=(type angle) {
     set(angle);
     return *this;
   }
@@ -81,12 +82,12 @@ namespace ics {
     rawData = checkInvalidAngle(raw); // throw std::out_of_range
   }
 
-  constexpr Angle::Angle(double calibration, double angle)
+  constexpr Angle::Angle(type calibration, type angle)
   : rawData {checkInvalidAngle(castToRaw(calibration, angle))}, // throw std::out_of_range
     rawCalibration {calibration}
   {}
 
-  constexpr Angle::rawType Angle::castToRaw(double calibration, double angle) noexcept {
+  constexpr Angle::rawType Angle::castToRaw(type calibration, type angle) noexcept {
     return static_cast<rawType>((calibration * angle) + MID);
   }
 
