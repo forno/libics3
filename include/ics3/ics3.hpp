6675027fd5c70731a38421b45247a1ced672acd4
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-01-27 10:39:40 +0900

    Add destructor for unique_ptr

--- include/ics3/ics3.hpp
+++ include/ics3/ics3.hpp
@@ -45,6 +45,7 @@ class ICS3
 {
 public:
   explicit ICS3(const std::string&, const Baudrate& = Baudrate::RATE115200());
+  ~ICS3() noexcept;
   Angle move(const ID&, Angle);
   Angle free(const ID&, Angle = Angle::newRadian());
   Parameter get(const ID&, const Parameter&);
