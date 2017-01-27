7706574a849fa681bb0714f7ecde517b2f1fbb76
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-01-27 10:35:06 +0900

    Revert "Remove my make_unique function"
    This reverts commit 400c5519ea5a3024e8eee40ee95e3d9b06217d18.

--- src/core.cpp
+++ src/core.cpp
@@ -33,6 +33,12 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "core.hpp"
 
+template<typename T, typename... Args>
+inline std::unique_ptr<T> make_unique(Args&&... args)
+{
+  return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
 ics::Core::Core(const std::string& path, speed_t baudrate)
 : fd {open(path.c_str(), O_RDWR | O_NOCTTY)},
   oldTio {}
@@ -83,7 +89,7 @@ ics::Core& ics::Core::operator=(Core&& rhs) noexcept
 
 std::unique_ptr<ics::Core> ics::Core::getCore(const std::string& path, speed_t baudrate)
 {
-  return std::unique_ptr<Core>(path, baudrate);
+  return make_unique<Core>(path, baudrate);
 }
 
 void ics::Core::communicate(const Container& tx, Container& rx)
