827020d45edbb469fee91792c90d1b95442cd587
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-02-01 14:57:05 +0900

    Order include headers

--- src/core.cpp
+++ src/core.cpp
@@ -25,10 +25,9 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 #include <cstring> // for memset
+#include <fcntl.h> // for open FLAGS
 #include <sstream> // for error massage
 #include <stdexcept>
-#include <unordered_map> // for cashe
-#include <fcntl.h> // for open FLAGS
 #include <unistd.h> // for tty checks
 
 #include "core.hpp"
